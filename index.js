166cc547b8a0d1eda819d771ef596e7e6aabf301
Aleksandar Simovic <alexander.simovic@gmail.com>
2018-12-09 17:33:06 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const AWS = require('aws-sdk'),
+  SES = new AWS.SES(),
+  processResponse = require('./process-response.js'),
+  FROM_EMAIL = process.env.FROM_EMAIL,
+  UTF8CHARSET = 'UTF-8';
+
+exports.handler = (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return Promise.resolve(processResponse(true));
+  }
+
+  if (!event.body) {
+    return Promise.resolve(processResponse(true, 'Please specify email parameters: toEmails, subject, and message ', 400));
+  }
+  const emailData = JSON.parse(event.body);
+
+  if (!emailData.toEmails || !Array.isArray(emailData.toEmails) || !emailData.subject || !emailData.message) {
+    return Promise.resolve(processResponse(true, 'Please specify email parameters: toEmails, subject and message', 400));
+  }
+
+  const destination = {
+    ToAddresses: emailData.toEmails
+  }
+
+  if (emailData.ccEmails) {
+    destination.CcAddresses = emailData.ccEmails;
+  }
+
+  const body = (emailData.message && isHTML(emailData.message)) ?
+    { Html: { Charset: UTF8CHARSET, Data: emailData.message } } :
+    { Text: { Charset: UTF8CHARSET, Data: emailData.message } };
+
+  const emailParams = {
+    Destination: destination,
+    Message: {
+      Body: body,
+      Subject: {
+        Charset: UTF8CHARSET,
+        Data: emailData.subject
+      }
+    },
+    Source: FROM_EMAIL
+  };
+
+  return SES.sendEmail(emailParams).promise()
+    .then(() => (processResponse(true)))
+    .catch(err => {
+      console.error(err, err.stack);
+      const errorResponse = `Error: Execution update, caused a SES error, please look at your logs.`;
+      return processResponse(IS_CORS, errorResponse, 500);
+    });
+};
+
+function isHTML(value) {
+  value = value.trim();
+  return value.startsWith('<') && value.endsWith('>') &&
+    (value.includes('<body') || value.includes('<div') || value.includes('<s') || value.includes('<h') || value.includes('<p'));
+}
